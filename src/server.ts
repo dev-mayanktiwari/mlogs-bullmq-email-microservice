9da3c1380d6e259048ffe5c9eb73431d1022a932
Mayank <mayanktiwari1212.mt@gmail.com>
2024-11-07 19:18:32 +0530

    feat: Added db logging

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+ 
+ 
 import generalQueueWorker from "./workers/generalQueueWorker";
 import passwordQueueWorker from "./workers/passwordQueueWorker";
 import deadLetterQueueWorker from "./workers/deadLetterQueueWorker";
